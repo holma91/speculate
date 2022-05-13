1b03da91271325f2b4f8d069e3da1f4a50457087
Alexander <alexanderholmberg91@gmail.com>
2022-05-13 21:32:27 +0200

    write page

--- frontend/pages/options/index.js
+++ frontend/pages/options/index.js
@@ -1,78 +1,250 @@
-import React from 'react';
-import regeneratorRuntime from 'regenerator-runtime'; // because of "ReferenceError: regeneratorRuntime is not defined"
+import Link from 'next/link';
 import styled from 'styled-components';
-import BuyNFT from '../../components/BuyNFT';
-import pairs from '../../data/pairs';
-import Table, { SelectColumnFilter } from '../../components/Table';
 
 export default function Options() {
-  const columns = React.useMemo(
-    () => [
-      {
-        Header: 'pair',
-        accessor: 'pair',
-        // Cell: AvatarCell,
-        addressAccessor: 'pairAddress',
-      },
-      {
-        Header: 'liquidity',
-        accessor: 'liquidityUsd',
-      },
-      {
-        Header: 'dex',
-        accessor: 'dex',
-        Filter: SelectColumnFilter,
-        filter: 'includes',
-        startValueFilter: 'all dexes',
-      },
-      {
-        Header: 'chain',
-        accessor: 'chain',
-        Filter: SelectColumnFilter,
-        filter: 'includes',
-        startValueFilter: 'all chains',
-      },
-      {
-        Header: 'token0',
-        accessor: 'token0Name',
-      },
-      {
-        Header: 'token1',
-        accessor: 'token1Name',
-      },
-      {
-        Header: 'created at (utc)',
-        accessor: 'createdAt',
-        id: 'createdAt',
-      },
-      {
-        Header: 'block explorer',
-        accessor: 'blockExplorerUrl',
-        isLink: true,
-      },
-      {
-        Header: 'chart',
-        accessor: 'dexscreenerUrl',
-        isLink: true,
-      },
-    ],
-    []
-  );
-
-  const initialState = {
-    sortBy: [
-      {
-        id: 'createdAt',
-        desc: true,
-      },
-    ],
-    pageSize: 10,
-  };
-
   return (
-    <div className="col-span-4 m-5">
-      sup
-      <Table columns={columns} data={pairs} initialState={initialState} />
-    </div>
+    <RecentContainer>
+      <RecentOptions>
+        <p>Recent Calls</p>
+
+        <OptionsHeader>
+          <span>Asset</span>
+          <span>Strike Price</span>
+          <span>Current Price</span>
+          <span>Right to buy</span>
+          <span>Expiry</span>
+          <span>1 ETH</span>
+        </OptionsHeader>
+        <Option>
+          <span>
+            <img src="https://prismic-io.s3.amazonaws.com/data-chain-link/7e81db43-5e57-406d-91d9-6f2df24901ca_ETH.svg" />
+            ETH/USD
+          </span>
+          <span>$3000</span>
+          <span>$2000</span>
+          <span>10 ETH</span>
+          <span>2022-12-14</span>
+          <span>1 ETH</span>
+        </Option>
+        <Option>
+          <span>
+            <img src="https://prismic-io.s3.amazonaws.com/data-chain-link/19a58483-b100-4d09-ab0d-7d221a491090_BTC.svg" />
+            BTC/USD
+          </span>
+          <span>$300000</span>
+          <span>$30000</span>
+          <span>5 BTC</span>
+          <span>2025-12-14</span>
+          <span>0.5 ETH</span>
+        </Option>
+        <Option>
+          <span>
+            <img src="https://images.prismic.io/data-chain-link/63137341-c4d1-4825-b284-b8a5a8436d15_ICON_AVAX.png?auto=compress,format" />
+            AVAX/USD
+          </span>
+          <span>$300</span>
+          <span>$30</span>
+          <span>10 AVAX</span>
+          <span>2023-06-14</span>
+          <span>0.05 ETH</span>
+        </Option>
+        <Option>
+          <span>
+            <img src="https://data-chain-link.cdn.prismic.io/data-chain-link/ad14983c-eec5-448e-b04c-d1396e644596_LINK.svg" />
+            LINK/USD
+          </span>
+          <span>$30</span>
+          <span>$5</span>
+          <span>100 LINK</span>
+          <span>2023-01-01</span>
+          <span>1 ETH</span>
+        </Option>
+        <Option>
+          <span>
+            <img src="https://images.prismic.io/data-chain-link/931ba23b-1755-46be-a466-73af2fcafaf1_ICON_SOL.png?auto=compress,format" />
+            SOL/USD
+          </span>
+          <span>$500</span>
+          <span>$50</span>
+          <span>1000 SOL</span>
+          <span>2024-01-01</span>
+          <span>1 ETH</span>
+        </Option>
+        <Option>
+          <span>
+            <img src="https://images.prismic.io/data-chain-link/63137341-c4d1-4825-b284-b8a5a8436d15_ICON_AVAX.png?auto=compress,format" />
+            AVAX/USD
+          </span>
+          <span>$300</span>
+          <span>$30</span>
+          <span>10 AVAX</span>
+          <span>2023-06-14</span>
+          <span>0.2 ETH</span>
+        </Option>
+      </RecentOptions>
+      <RecentOptions>
+        <p>Recent Puts</p>
+        <Option>
+          <span>
+            <img src="https://prismic-io.s3.amazonaws.com/data-chain-link/7e81db43-5e57-406d-91d9-6f2df24901ca_ETH.svg" />
+            ETH/USD
+          </span>
+          <span>$3000</span>
+          <span>2022-07-01</span>
+        </Option>
+        <Option>
+          <span>
+            <img src="https://images.prismic.io/data-chain-link/931ba23b-1755-46be-a466-73af2fcafaf1_ICON_SOL.png?auto=compress,format" />
+            SOL/USD
+          </span>
+          <span>$100</span>
+          <span>2022-11-14</span>
+        </Option>
+        <Option>
+          <span>
+            <img src="https://data-chain-link.cdn.prismic.io/data-chain-link/53abc2bf-a078-40d5-9f46-835968837ee4_DOT.svg" />
+            DOT/USD
+          </span>
+          <span>$8</span>
+          <span>2022-10-14</span>
+        </Option>
+        <Option>
+          <span>
+            <img src="https://prismic-io.s3.amazonaws.com/data-chain-link/19a58483-b100-4d09-ab0d-7d221a491090_BTC.svg" />
+            BTC/USD
+          </span>
+          <span>$30000</span>
+          <span>2022-11-19</span>
+        </Option>
+        <Option>
+          <span>
+            <img src="https://prismic-io.s3.amazonaws.com/data-chain-link/7e81db43-5e57-406d-91d9-6f2df24901ca_ETH.svg" />
+            ETH/USD
+          </span>
+          <span>$2200</span>
+          <span>2023-01-17</span>
+        </Option>
+        <Option>
+          <span>
+            <img src="https://data-chain-link.cdn.prismic.io/data-chain-link/53abc2bf-a078-40d5-9f46-835968837ee4_DOT.svg" />
+            DOT/USD
+          </span>
+          <span>$8</span>
+          <span>2023-04-16</span>
+        </Option>
+      </RecentOptions>
+    </RecentContainer>
   );
 }
+
+const RecentContainer = styled.div`
+  margin: 40px;
+  display: flex;
+  flex-direction: column;
+  gap: 50px;
+
+  @media screen and (max-width: 1000px) {
+    gap: 10px;
+  }
+`;
+
+const RecentOptions = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  /* width: 450px; */
+
+  p {
+    font-size: 20px;
+    font-weight: 600;
+  }
+
+  a {
+    padding: 10px;
+    font-weight: 500;
+  }
+`;
+
+const OptionsHeader = styled.div`
+  padding: 10px;
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  gap: 5px;
+  justify-content: space-between;
+  align-items: center;
+  /* border: 1px solid #ecedef; */
+  border-radius: 6px;
+  cursor: pointer;
+  /* box-shadow: 0 8px 24px -16px rgba(12, 22, 44, 0.32); */
+
+  span {
+    display: flex;
+    align-items: center;
+    gap: 6px;
+  }
+`;
+
+const Option = styled.div`
+  padding: 10px;
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  gap: 5px;
+  justify-content: space-between;
+  align-items: center;
+  border: 1px solid #ecedef;
+  border-radius: 6px;
+  cursor: pointer;
+  box-shadow: 0 8px 24px -16px rgba(12, 22, 44, 0.32);
+
+  img {
+    width: 19px;
+    height: 19px;
+  }
+
+  span {
+    display: flex;
+    align-items: center;
+    gap: 6px;
+  }
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 0;
+  margin: 0;
+
+  h1 {
+    margin-top: 50px;
+    margin-bottom: 10px;
+    font-size: 70px;
+  }
+
+  @media screen and (max-width: 1000px) {
+    h1 {
+      font-size: 48px;
+    }
+  }
+
+  @media screen and (max-width: 500px) {
+    h1 {
+      font-size: 40px;
+    }
+  }
+`;
+
+const Menu = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  a {
+    font-size: 32px;
+    margin: 0 7px;
+
+    :hover {
+      color: #0f6cf7;
+    }
+  }
+`;
